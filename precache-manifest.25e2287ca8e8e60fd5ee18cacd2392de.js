8f4cb07380eea4460dfb84df61c0946ba149baab
Hok Laam Cheng <hoklaam.cheng@gmail.com>
2020-01-05 17:09:14 -0500

    Updates

---
+++ precache-manifest.25e2287ca8e8e60fd5ee18cacd2392de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941ef9589ef8f21dd0a0696bb35710f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03de588e3c86ec6d2a5c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "531a16c76918a9c53487",
+    "url": "/static/js/2.b05d032b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b05d032b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03de588e3c86ec6d2a5c",
+    "url": "/static/js/main.f8387a73.chunk.js"
+  },
+  {
+    "revision": "617fa224444cd42981bb",
+    "url": "/static/js/runtime-main.ef1c2933.js"
+  }
+]);
